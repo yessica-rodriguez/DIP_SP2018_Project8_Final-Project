25cf6de37da8bd0e9c821a8e8ca2df55cad4c220
yessica-rodriguez <32173048+yessica-rodriguez@users.noreply.github.com>
2018-05-07 22:32:38 -0500

    Update Features.java
    I added a function that compares the features and a classification function that finds the smallest distance.

--- ImageCapture/src/recog/Features.java
+++ ImageCapture/src/recog/Features.java
@@ -12,4 +12,31 @@ package recog;
 public class Features {
     // features for the key
     
+    public double distance_feature_vector(double[] piece, double[] input){
+        int size = piece.length;
+        double distance = 0.0;
+        double[] result_vector = new double[size];
+        for(int i = 0; i < size; i++){
+            result_vector[i] = input[i] - piece[i];
+        }
+        for(int i = 0; i < size; i++){
+            distance += (result_vector[i] * result_vector[i]);
+        }
+        distance = Math.sqrt(distance);
+        return distance;
+    }
+    
+    public String classification(double[] distances){
+        Database d = new Database();
+        int class_num = 0;
+        double min = distances[0];
+        for(int i = 1; i < distances.length; i++){
+            if(distances[i] < min){
+                min = distances[i];
+                class_num = i;
+            }
+        }
+        return d.getKeyOwnerInfo(class_num+1);
+    }
+    
 }
